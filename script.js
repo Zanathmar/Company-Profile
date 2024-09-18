da9ac77730ae8d3dce946d712cece9e4dc3c25ce
Zanathmar <izzanathmar.m@gmail.com>
2024-09-18 20:28:50 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let navMenu = document.getElementById("main-nav")
+
+function openMenu() {
+    navMenu.classList.add("responsive_nav");
+}
+function closeMenu() {
+    navMenu.classList.remove("responsive_nav")
+    
+}
